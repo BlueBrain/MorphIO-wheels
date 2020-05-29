45c45a167c30a57482199a1755b4619c10b5ac6e
Benoit Coste <benoit.coste@epfl.ch>
2020-05-29 13:19:33 -0700

    bring the conda dll

--- delocate.py
+++ delocate.py
@@ -4,11 +4,14 @@ import sys
 import shutil
 
 
-def delocate_hdf5(folder):
-    import h5py
-    hdf5_lib = Path(h5py.__file__).parent / 'hdf5.dll'
+def delocate_hdf5(folder, dll_folder):
+    hdf5_lib = Path(dll_folder).parent / 'hdf5.dll'
     if not hdf5_lib.exists():
         raise Exception(f'{hdf5_lib} not found !')
+
+    zlib_lib = Path(dll_folder).parent / 'zlib.dll'
+    if not zlib_lib.exists():
+        raise Exception(f'{zlib_lib} not found !')
 
     if not folder.exists():
         raise Exception(f'{folder} not found !')
@@ -18,8 +21,9 @@ def delocate_hdf5(folder):
     print("filename: {}".format(filename))
     with zipfile.ZipFile(filename,'a') as zip:
         zip.write(hdf5_lib, 'morphio/hdf5.dll')
+        zip.write(zlib_lib, 'morphio/zlib.dll')
 
 if __name__=='__main__':
-    if len(sys.argv) < 2:
-        raise Exception('Missing argument WHEEL_FOLDER')
-    delocate_hdf5(Path(sys.argv[1]))
+    if len(sys.argv) < 3:
+        raise Exception('Usage: WHEEL_FOLDER DLL_FOLDER')
+    delocate_hdf5(Path(sys.argv[1]), Path(sys.argv[2]))
