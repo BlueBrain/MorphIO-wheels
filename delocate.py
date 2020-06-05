d8b5c9b9c24ddc1feeb6ce6c7373ee9f6d0c93f0
Benoit Coste <benoit.coste@protonmail.com>
2020-06-05 05:39:20 -0700

    Upload Windows wheels to test PyPi (#11)
    The following DLL are being packaged with the wheel:
    - hdf5.dll
    - zlib.dll
    - msvcp140.dll
    
    msvcp140.dll is "Microsoft Visual C ++ Redistributable package". From what I could understand it is a runtime requirement for C++ apps built with Visual Studio. I am not the first one shipping it with its wheel: https://github.com/JSBSim-Team/jsbsim/pull/159/files

---
+++ delocate.py
@@ -0,0 +1,28 @@
+import sys
+import zipfile
+from pathlib import Path
+
+
+def delocate_hdf5(wheel_folder, dll_folder):
+    '''Add hdf5.dll, zlib.dll and msvcp140.dll to the wheel
+
+    Args:
+        wheel_folder: the folder containing the wheel
+        dll_folder: the folder containing the dlls
+    '''
+    if not wheel_folder.exists():
+        raise Exception(f'{wheel_folder} not found !')
+
+    filename = next(wheel_folder.rglob('*whl'))
+    with zipfile.ZipFile(filename, 'a') as zip:
+        for dll in ['zlib', 'hdf5', 'msvcp140']:
+            dll_lib = Path(dll_folder, f'{dll}.dll')
+            if not dll_lib.exists():
+                raise Exception(f'{dll_lib} not found !')
+            zip.write(dll_lib, f'morphio/{dll}.dll')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        raise Exception('Usage: python delocate.py WHEEL_FOLDER DLL_FOLDER')
+    delocate_hdf5(Path(sys.argv[1]), Path(sys.argv[2]))
